471d4a53aa456476ad902f51e630b28fafe571ac
Rayan Bahri <114908701+Rynbh1@users.noreply.github.com>
2026-02-17 18:00:16 +0100

    code

--- back/services/auth_logic.py
+++ back/services/auth_logic.py
@@ -34,14 +34,17 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        user = db.query(models.User).filter(models.User.email == username).first()
-        if user is None:
-            raise credentials_exception
-            
-        return user
-
-    except JWTError:
+    except JWTError: # Maintenant défini grâce à l'import
         raise credentials_exception
+
+    # Recherche de l'utilisateur en base de données
+    user = db.query(User).filter(User.email == username).first()
+    
+    if user is None:
+        raise credentials_exception
+        
+    return user
+
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
